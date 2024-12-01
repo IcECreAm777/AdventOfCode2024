20eeea2020a661d11f2e8ad9dddb2ab7a59d36dd
IcECreAm777 <31211782+IcECreAm777@users.noreply.github.com>
2024-12-01 17:10:21 +0100

    added command line argument to control which day(s) are being executed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AdventOfCode2024
-Repository for the Advent of Code 2024
+Repository for the Advent of Code 2024. This year done in Rust.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
+use std::env;
+
 mod day_one;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        run_every_day();
+        return;
+    }
+
+    let day: i64 = match args[1].parse::<i64>() {
+        Ok(num) => num,
+        Err(num) => {
+            println!("{}", num);
+            0
+        },
+    };
+
+    match day {
+        1 => day_one(),
+        _ => run_every_day(),
+    }
+}
+
+fn run_every_day() {
+    println!("running every day...");
+
+    day_one();
+
+    println!("finished executing every day!")
+}
+
+fn day_one() {
     println!("running day one...");
-
     day_one::run_day_01();
-
     println!("finished day one!");
 }
