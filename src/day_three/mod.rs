f0591f19e8b96ef689cf25725e2511226a21c969
IcECreAm777 <31211782+IcECreAm777@users.noreply.github.com>
2024-12-05 02:00:24 +0100

    finished day 3

--- src/day_three/mod.rs
+++ src/day_three/mod.rs
@@ -3,10 +3,12 @@ use std::fs::read_to_string;
 
 pub fn run_day_03() {
     let re_task01 = Regex::new(r"mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)").unwrap();
-    let re_task02 = Regex::new(r"mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)|(?<do>do\(\))|(?<dont>don't\(\))").unwrap();
+    let re_task02 = Regex::new(r"mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)|do\(\)|don't\(\)").unwrap();
     
     let mut first_result: i64 = 0;
     let mut second_result: i64 = 0;
+
+    let mut active = true;
 
     for line in read_to_string("src\\day_three\\input.txt").unwrap().lines() {
         for cap in re_task01.captures_iter(line) {
@@ -16,29 +18,20 @@ pub fn run_day_03() {
             first_result += first * second;
         }
 
-        let mut active = true;
-        for cap in re_task02.captures_iter(line) {
-            match cap.name("do") {
-                Some(_) => {
-                    active = true;
-                    continue;
-                },
-                None => ()
-            }
+        for cap in re_task02.find_iter(line) {
+            match cap.as_str() {
+                "do()" => {active = true;}
+                "don't()" => {active = false;}
+                _ => {
+                    if active {
+                        if let Some(mul) = re_task01.captures(cap.as_str()) {
+                            let first = &mul["first"].parse::<i64>().unwrap();
+                            let second = &mul["second"].parse::<i64>().unwrap();
 
-            match cap.name("dont") {
-                Some(_) => {
-                    active = false;
-                    continue;
-                },
-                None => ()
-            }
-
-            if active {
-                let first = &cap["first"].parse::<i64>().unwrap();
-                let second = &cap["second"].parse::<i64>().unwrap();
-
-                second_result += first * second;
+                            second_result += first * second;
+                        }
+                    }
+                }
             }
         }
     }
