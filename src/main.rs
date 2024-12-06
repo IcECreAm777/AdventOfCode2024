2fa57652b9484c6da8e3c98945350ec1b6a308d7
IcECreAm777 <31211782+IcECreAm777@users.noreply.github.com>
2024-12-06 18:51:05 +0100

    day 6 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::env;
+use std::time::Instant;
 
 mod day_one;
 mod day_two;
 mod day_three;
 mod day_four;
 mod day_five;
+mod day_six;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -28,6 +30,7 @@ fn main() {
         3 => day_three(),
         4 => day_four(),
         5 => day_five(),
+        6 => day_six(),
         _ => run_every_day(),
     }
 }
@@ -40,31 +43,49 @@ fn run_every_day() {
     day_three();
     day_four();
     day_five();
+    day_six();
 
     println!("finished executing every day!")
 }
 
 fn day_one() {
     println!("running day one...");
+    let start = Instant::now();
     day_one::run_day_01();
+    println!("executing day took {} ms", start.elapsed().as_millis());
 }
 
 fn day_two() {
     println!("running day two...");
+    let start = Instant::now();
     day_two::run_day_02();
+    println!("executing day took {} ms", start.elapsed().as_millis());
 }
 
 fn day_three() {
     println!("running day three...");
+    let start = Instant::now();
     day_three::run_day_03();
+    println!("executing day took {} ms", start.elapsed().as_millis());
 }
 
 fn day_four() {
     println!("running day four...");
+    let start = Instant::now();
     day_four::run_day_04();
+    println!("executing day took {} ms", start.elapsed().as_millis());
 }
 
 fn day_five() {
     println!("running day five...");
+    let start = Instant::now();
     day_five::run_day_05();
+    println!("executing day took {} ms", start.elapsed().as_millis());
+}
+
+fn day_six() {
+    println!("running day six...");
+    let start = Instant::now();
+    day_six::run_day_06();
+    println!("executing day took {} ms", start.elapsed().as_millis());
 }
