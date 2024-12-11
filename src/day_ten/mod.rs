ae9bb7c9ede1abbb599867b0c0830e03602bff7e
IcECreAm777 <31211782+IcECreAm777@users.noreply.github.com>
2024-12-11 19:16:23 +0100

    day 10 part 2

--- src/day_ten/mod.rs
+++ src/day_ten/mod.rs
@@ -2,6 +2,16 @@ use std::fs::read_to_string;
 
 pub fn run_day_10() {
     let path = "src\\day_ten\\input.txt";
+    
+    let (area, zero_index) = parse_input(path);
+    let task_one_result = task01(&area, zero_index.clone());
+    let task_two_result = task02(&area, zero_index.clone());
+
+    println!("\ttask one result: {}", task_one_result);
+    println!("\ttask two result: {}", task_two_result);
+}
+
+fn parse_input(path: &str) -> (Vec<Vec<i8>>, Vec<(usize, usize)>) {
     let mut area: Vec<Vec<i8>> = vec![];
     let mut zero_index: Vec<(usize, usize)> = vec![];
 
@@ -26,6 +36,10 @@ pub fn run_day_10() {
         row_index += 1;
     }
 
+    (area, zero_index)
+}
+
+fn task01(area: &Vec<Vec<i8>>, zero_index: Vec<(usize, usize)>) -> i32 {
     let mut result = 0;
     for zero in zero_index {
         let mut index = 0;
@@ -47,7 +61,17 @@ pub fn run_day_10() {
         result += next.len() as i32;
     } 
 
-    println!("\ttask one result: {}", result);
+    result
+}
+
+fn task02(area: &Vec<Vec<i8>>, zero_index: Vec<(usize, usize)>) -> i32 {
+    let mut result = 0;
+
+    for zero in zero_index {
+        result += traverse_to_next(area, zero, 0);
+    }
+
+    result
 }
 
 fn get_next_index(area: &Vec<Vec<i8>>, coords: (usize, usize), index: i8) -> Vec<(usize, usize)> {
@@ -70,4 +94,35 @@ fn get_next_index(area: &Vec<Vec<i8>>, coords: (usize, usize), index: i8) -> Vec
     }
 
     next
+}
+
+fn traverse_to_next(area: &Vec<Vec<i8>>, coords: (usize, usize), index: i8) -> i32 {
+    let current = area[coords.0][coords.1];
+    if current != index {
+        return 0
+    }
+
+    if index == 9 {
+        return 1;
+    }
+
+    let mut result = 0;
+
+    if coords.0 > 0 && area[coords.0 - 1][coords.1] == index + 1 {
+        result += traverse_to_next(area, (coords.0 - 1, coords.1), index + 1);
+    } 
+
+    if coords.1 > 0 && area[coords.0][coords.1-1] == index + 1 {
+        result += traverse_to_next(area, (coords.0, coords.1-1), index + 1);
+    }
+
+    if coords.0 < area.len()-1 && area[coords.0 + 1][coords.1] == index + 1 {
+        result += traverse_to_next(area, (coords.0 + 1, coords.1), index + 1);
+    }
+
+    if coords.1 < area[0].len()-1 && area[coords.0][coords.1+1] == index + 1 {
+        result += traverse_to_next(area, (coords.0, coords.1 + 1), index + 1);
+    }
+
+    result
 }
