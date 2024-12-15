53f143462a0b0d4f3cedb919966e55248a5127ed
IcECreAm777 <31211782+IcECreAm777@users.noreply.github.com>
2024-12-15 20:39:28 +0100

    cleanup day 13

--- src/day_thirteen/mod.rs
+++ src/day_thirteen/mod.rs
@@ -1,5 +1,9 @@
 use std::fs::read_to_string;
 
+mod crane;
+
+use crane::Crane;
+
 pub fn run_day_13() {
     let path = r"src\day_thirteen\input.txt";
 
@@ -52,59 +56,4 @@ pub fn run_day_13() {
 
     println!("\ttask one result: {}", task_one_result);
     println!("\ttask one result: {}", task_two_result);
-}
-
-#[derive(Debug)]
-pub struct Crane {
-    a_x: u64,
-    a_y: u64,
-    b_x: u64,
-    b_y: u64,
-    r_x: u64,
-    r_y: u64
-}
-
-impl Crane {
-    pub fn new() -> Crane {
-        Crane { a_x: 0, a_y: 0, b_x: 0, b_y: 0, r_x: 0, r_y: 0 }
-    }
-
-    pub fn set_a_button(&mut self, x: u64, y: u64) {
-        self.a_x = x;
-        self.a_y = y;
-    }
-
-    pub fn set_b_button(&mut self, x: u64, y: u64) {
-        self.b_x = x;
-        self.b_y = y;
-    }
-
-    pub fn set_result(&mut self, x:u64, y: u64) {
-        self.r_x = x;
-        self.r_y = y;
-    }
-
-    pub fn correct_result(&mut self) {
-        self.r_x += 10000000000000;
-        self.r_y += 10000000000000;
-    }
-
-    pub fn solve(&self) -> u64 {
-        let det_a = (self.a_x * self.b_y) as i128 - (self.b_x * self.a_y) as i128;
-        let det_one = (self.r_x * self.b_y) as i128 - (self.b_x * self.r_y) as i128;
-        let det_two = (self.a_x * self.r_y) as i128 - (self.r_x * self.a_y) as i128;
-
-        if det_one % det_a != 0 || det_two % det_a != 0 {
-            return 0
-        }
-
-        let solution_one = det_one / det_a;
-        let solution_two = det_two / det_a;
-
-        if solution_one < 0 || solution_two < 0 {
-            return 0;
-        }
-
-        solution_one as u64 * 3 + solution_two as u64
-    }
 }
